043ed6c691915849d824208bb46f2dace19f4370
hermannsw <symsapiens@gmail.com>
2020-05-15 17:42:06 +0900

    create presenter

---
+++ src/presenter/common_presenter.py
@@ -0,0 +1,50 @@
+class RequestSucceededPresenter:
+    @staticmethod
+    def response(message):
+        return {
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            },
+            'statusCode': '200',
+            'body': "{\"message\":\"" + message + "\"}",
+        }
+
+
+class BadRequestPresenter:
+    @staticmethod
+    def response(message):
+        return {
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            },
+            'statusCode': '400',
+            'body': "{\"message\":\"" + message + "\"}",
+        }
+
+
+class NotFoundPresenter:
+    @staticmethod
+    def response(message):
+        return {
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            },
+            'statusCode': '404',
+            'body': "{\"message\":\"" + message + "\"}",
+        }
+
+
+class MethodNotAllowedPresenter:
+    @staticmethod
+    def response(message):
+        return {
+            'headers': {
+                'Access-Control-Allow-Origin': '*',
+                'Content-Type': 'application/json',
+            },
+            'statusCode': '405',
+            'body': "{\"message\":\"" + message + "\"}",
+        }
